405ec2ac1dc475d9178c603e2b2d929575fa38d5
Moacir Moraes <moacir.viana@gmail.com>
2025-03-12 14:40:03 -0400

    configuração do Cors

--- src/main/java/br/jus/tream/lontra/config/SecurityConfig.java
+++ src/main/java/br/jus/tream/lontra/config/SecurityConfig.java
@@ -5,7 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -14,9 +21,9 @@ public class SecurityConfig {
   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     
     return http
-   		.csrf(csrf -> csrf.disable())
-   		.cors(cors -> cors.disable())
-  		.authorizeHttpRequests(authz -> authz.requestMatchers("/**").permitAll()).build();
+        .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+        .csrf(AbstractHttpConfigurer::disable)
+  		.authorizeHttpRequests(auths -> auths.requestMatchers("/**").permitAll()).build();
   		//.addFilterBefore(new AuthenticationFilter(),  UsernamePasswordAuthenticationFilter.class).build();
     }
 
@@ -24,5 +31,16 @@ public class SecurityConfig {
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers("/h2-console/**");
     }
-  
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("*"));
+        configuration.setAllowedMethods(Arrays.asList("*"));
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
